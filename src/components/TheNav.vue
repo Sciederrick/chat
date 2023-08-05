9abcbde773647b3c049f716c5482c9fe6c27b539
Derrick Mbarani <derrickmbarani@gmail.com>
2023-08-05 21:43:48 +0300

    add tailwind css

---
+++ src/components/TheNav.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+<template>
+    <header>
+        <img src="" alt="logo" />
+        <img src="" alt="profile avatar" />
+    </header>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import TheNav from '../components/TheNav.vue'
+import TheFooter from '../components/TheFooter.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <TheNav />
+    <h1 class="text-xl">Hello </h1>
+    <div class="bg-red-500 h-48 w-48">dfdf</div>
+    <TheFooter />
   </main>
 </template>
